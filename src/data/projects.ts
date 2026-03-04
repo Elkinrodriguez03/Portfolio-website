0466e69e2808b9044daa852219c98a957a532427
Elkin <elkinrodriguez03@gmail.com>
2026-03-03 21:21:34 -0500

    chore(config): set up typed data layer and modernize TS/build tooling

---
+++ src/data/projects.ts
@@ -0,0 +1,10 @@
+export type Project = {
+  name: string;
+  description: string;
+  image: string;
+  github: string;
+  link: string;
+};
+
+export const projects: Project[] = [];
+
---
+++ src/data/socialLinks.ts
@@ -0,0 +1,7 @@
+export type SocialLink = {
+  label: string;
+  href: string;
+};
+
+export const socialLinks: SocialLink[] = [];
+
